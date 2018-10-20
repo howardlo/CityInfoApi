3ccd83ef715ce2d2b9f4f95bbdbdd5862faabb72
Howard <howardlo@outlook.com>
2018-10-20 11:06:06 -0700

    Add create a point of interest

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [Route("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult( CitiesDataStore.Current.Cities );
+        }
+
+        [Route("api/cities/{id}")]
+        public JsonResult GetCity(int Id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.Where(e => e.Id == Id));
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointOfInterest.cs
@@ -0,0 +1,76 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/")]
+    public class PointsOfInterest : Controller
+    {
+        [HttpGet("{cityId}/PointsOfInterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/PointsOfInterest/{id}", Name = "GetPointOfInterest")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poi = city.PointsOfInterest.SingleOrDefault(p => p.Id == id);
+            if (poi == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(poi);
+        }
+
+        [Route("{cityId}/PointsOfInterest")]
+        [HttpPost()]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            var b = Request.Body;
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new
+            {
+                cityId = cityId,
+                id = finalPointOfInterest.Id
+            }, finalPointOfInterest);
+        }
+    }
+}
