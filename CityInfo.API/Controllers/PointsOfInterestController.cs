74caf2142e9a0eb1e4f6ac6faae3d36383948576
Howard <howardlo@outlook.com>
2018-10-21 08:41:53 -0700

    Add write to log file (nlog.config must have Copy Always property)

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,7 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,21 @@ namespace CityInfo.API.Controllers
     [Route("api/cities/")]
     public class PointsOfInterestController : Controller
     {
+        private ILogger<PointsOfInterestController> _logger;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet("{cityId}/PointsOfInterest")]
         public IActionResult GetPointsOfInterest(int cityId)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
+                // _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest.");
+                _logger.LogCritical($"City with id {cityId} wasn't found when accessing points of interest.");
                 return NotFound();
             }
             return Ok(city.PointsOfInterest);
@@ -28,6 +38,7 @@ namespace CityInfo.API.Controllers
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
             if (city == null)
             {
+                _logger.LogInformation($"City with id {cityId} wasn't found when accessing points of interest.");
                 return NotFound();
             }
 
@@ -189,9 +200,6 @@ namespace CityInfo.API.Controllers
             city.PointsOfInterest.Remove(pointOfInterestFromStore);
 
             return NoContent();
-
         }
     }
-
-
 }
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace CityInfo.API
 {
@@ -22,7 +23,12 @@ namespace CityInfo.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            // loggerFactory.AddConsole();
+            // loggerFactory.AddDebug();
+            
+            // shortcut below
+            // loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+            loggerFactory.AddNLog();
 
             if (env.IsDevelopment())
             {
